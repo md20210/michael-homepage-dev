b9b1b8925438be1a9dffc7fef818afa4e2e0b055
Michael Dabrock <michael.dabrock@web.de>
2025-06-21 11:42:08 +0200

    Re-fix merge conflict in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -14,21 +13,4 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: true
   }
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
->>>>>>> b79e33e740a046e7e19e9b3afcde5e892e7c6691
 })
