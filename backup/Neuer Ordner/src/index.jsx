92d4434835094114f871d557321e26b276fd7723
Michael Dabrock <michael.dabrock@web.de>
2025-06-21 11:05:23 +0200

    Add new files and delete Resume Eng.pdf

---
+++ backup/Neuer Ordner/src/index.jsx
@@ -0,0 +1,12 @@
+// src/index.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+// Create root and render app
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
