555010bc6a3bf53519c9a396a8d7036d70d45a47
Michael Dabrock <michael.dabrock@web.de>
2025-06-28 18:15:29 +0200

    Test: Switch to .mjs ES modules

---
+++ api/grok.mjs
@@ -0,0 +1,7 @@
+﻿export default function handler(req, res) {
+    res.status(200).json({
+        hello: "world from mjs",
+        method: req.method,
+        timestamp: new Date().toISOString()
+    });
+}
